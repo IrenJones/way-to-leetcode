98c3fc9ceb65d9f46a5ca39d16147940c08faf76
IrenJones <nor.iren@yandex.ru>
2021-02-11 12:27:07 -0800

    change icons

--- src/components/utilities/Footer.js
+++ src/components/utilities/Footer.js
@@ -1,15 +1,19 @@
 import React from "react";
 import "./css/Footer.css"
+import codeLogo from "./code.svg";
+import ghLogo from "./gh.svg";
 
 function Footer() {
   return (
-    <div className = "footer">
-            <div className = "footer-content">
-                <a>Leetcode profile</a>
+    <div className="footer">
+            <div className="footer-content">
+                <img className="imgSizer" src={codeLogo}></img>
+                <a href="https://leetcode.com/emma_renren/">Leetcode profile</a>
                 <span></span>
                  |
-                <span></span> 
-                <a>Github project link</a>
+                <span></span>
+                <img className="imgSizer" src={ghLogo}></img> 
+                <a href="https://github.com/IrenJones/way-to-leetcode">Github project link</a>
             </div>
     </div>
   );
